5f075f788a1d7bd557a2bb1d735e11b5828ba4f9
koma <amok6u@gmail.com>
2017-04-22 21:06:02 +0300

    build

--- api/server/boot/root.js
+++ api/server/boot/root.js
@@ -1,8 +1,13 @@
 'use strict';
 
+const path = require('path');
+const express = require('express');
+
+const STATIC_DIR = path.join(__dirname, '../../../webapp/build');
+
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  router.get('/*', express.static(STATIC_DIR));
   server.use(router);
 };
